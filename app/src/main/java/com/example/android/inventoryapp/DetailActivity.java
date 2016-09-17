5374f7787b59cfc44f3bd9a96a49254a44adade5
Travis Armstrong <travisdarmstrong@gmail.com>
2016-09-17 15:18:56 -0700

    rough implementation of detail layout

--- app/src/main/java/com/example/android/inventoryapp/DetailActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/DetailActivity.java
@@ -3,6 +3,7 @@ package com.example.android.inventoryapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 /**
  * Created by travis on 9/11/16.
@@ -13,5 +14,12 @@ public class DetailActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the detail menu
+        getMenuInflater().inflate(R.menu.detail_menu, menu);
+        return true;
     }
 }
